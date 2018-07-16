4ec7ad837a6f35924ebae6dd6bf42cbcb16b11b9
aujanand <anand.anuj91@gmail.com>
2018-07-17 00:12:21 +0530

    anujanand commit

---
+++ src/main/java/anuj4/anand4/App.java
@@ -0,0 +1,13 @@
+package anuj4.anand4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
